03ad82b4b6fe5c9b3996557ac4aec285fac29406
Miguel-INIF <Miguel_ING_Amador@outlook.com>
2018-02-14 21:44:49 -0600

    Procesamiento de mapas

--- roya.py
+++ roya.py
@@ -1,7 +1,11 @@
-
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
 from api import claves
+import pandas as pd 
+import numpy as np
+import shapefile 
 import ftplib 
-import time 
+import time
 import sys
 import os
 
@@ -68,26 +72,62 @@ class DescargarArchivos(Login):
 		if os.path.exists('{}'.format(fecha)):
 			os.chdir('{}'.format(fecha))
 			for i in range(1, 6):
-				print("Descargando el archivo d{}.txt".format(i))
+				print("Descargando el Archivo d{}.txt".format(i))
 				self.ftp.retrbinary('RETR d{}.txt'.format(i),open('d{}.txt'.format(i),'wb').write)
 		else:
 			os.mkdir('{}'.format(fecha))
 			os.chdir('{}'.format(fecha))
 			for i in range(1, 6):
-				print("Descargando el archivo d{}.txt".format(i))
+				print("Descargando el Archivo d{}.txt".format(i))
 				self.ftp.retrbinary('RETR d{}.txt'.format(i),open('d{}.txt'.format(i), 'wb').write)
 		self.ftp.quit()
 		os.chdir('../..')
 
-class ProcesadoInformacion():
-	pass
+class CreacionMapas:
+	def Mapas(self, fecha):
+		'''La siguiente funcion procesara y filtrara la informacion para 
+		generar los respectivos mapas de los diferentes cambios climatologicos'''
+		os.chdir('data/{}'.format(fecha))
+		VariablesClima = ('Tpro','Dpoint','Noch_fres')
+		for i in range(1, 3):
+			print('Procesando la informacion del Documento d{}.txt'.format(i))
+			data = pd.read_csv('d{}.txt'.format(i))
+			for j in range(len(VariablesClima)):
+				print('Procesando Mapa {} del {}'.format(VariablesClima[j], fecha))
+				Tierra = data.loc[data['WprSoil10_40'] <= 99]
+				x , y = 'Long', 'Lat' 
+				Long = np.array(data['{}'.format(x)])
+				Lat = np.array(data['{}'.format(y)])
+				if i == 0:
+					procesado = Tierra.loc[Tierra['{}'.format(VariablesClima[j])] >=25]
+					procesado = procesado.loc[procesado['{}'.format(VariablesClima[j] <= 30)]]
+				elif i == 1:
+					procesado = Tierra.loc[Tierra['{}'.format(VariablesClima[j])] > 5]
+				elif i == 2:
+					procesado = Tierra.loc[(Tierra['Tmax'] - Tierra['Tmin']) >=15]
+					procesado = Tierra.loc[(Tierra['Tmax'] - Tierra['Tmin']) <=20]
 
-class Mapas():
-	pass
-	
+				Eje_x = np.array(procesado['{}'.format(x)])
+				Eje_y = np.array(procesado['{}'.format(y)])
+
+				map = Basemap(projection='mill',
+								resolution='l',
+								area_thresh=0.01,
+								llcrnrlon=Long.min(), llcrnrlat=Lat.min(),
+								urcrnrlon=Long.max(), urcrnrlat=Lat.max())
+				x1, x2 = map(Eje_x, Eje_y)
+				map.scatter(x1, x2, marker='.',color='#0000FF')
+				os.chdir('../..')
+				map.readshapefile('shapes/Estados','mill')
+				od.chdir('data/{}'.format(fecha))
+				plt.title('{} del {}'.format(VariablesClima[j], fecha))
+				plt.savefig('{} del {}.png'.format(VariablesClima[j],fecha))
+
 if __name__ == "__main__":
 	fecha = Fecha().obtencionFecha()
 	FehasArreglo = ArregloFecha().fechas(fecha)
-	print("El proceso de descarga a comenzado en la carpeta {}".format(fecha))
-	print("Espera un poco a que finalice el proceso...")
+	print('El proceso de descarga de archivo a empezado')
+	print('Espera algunos minutos para que el proceso llegue a finalizar...')
 	DescargarArchivos().descDocs(fecha)
+	print('------------------------------')
+	CreacionMapas().Mapas(fecha)
