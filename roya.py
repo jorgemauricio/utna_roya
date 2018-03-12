21e517a91038c3b4200846a253da4308818187aa
Miguel-INIF <Miguel_ING_Amador@outlook.com>
2018-03-11 22:35:07 -0600

    Creacion de mapas

--- roya.py
+++ roya.py
@@ -1,5 +1,5 @@
-#from mpl_toolkits.basemap import Basemap
-#import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
 from api import claves
 import pandas as pd 
 import numpy as np
@@ -8,7 +8,14 @@ import ftplib
 import time
 import sys
 import os
-listaParametros = [25,30,5]
+
+#Variables Globales 
+colores = ['#00FF00','#00FF00','#00FF00','#00FF00','#FFFF00','#FFFF00','#FFFF00','#FF8000','#FF8000','#FF0000']
+rangos = ('11111', '11110', '01111', '11100', '01110', '00111', '11000', '01100', '00110', '00011')
+grado = (10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+var = ['Tpro', 'Dpoint', 'NocFres']
+Rango = [25,30,5,15,20]
+lista = []
 
 class Login():
 	'''Clase constructura.'''
@@ -62,12 +69,20 @@ class DescargarArchivos(Login):
 	def __init__(self):
 		super().__init__()
 
-	def descDocs(self, fecha):
-		'''Descargar los 5 archivos respectivo a la fecha
+	def descarga(self):
+		'''Metodo para descar los 5 dias respectivo a la fecha 
 		ingresada por parametro'''
 		self.ftp = ftplib.FTP(self.clv.ip)
 		self.ftp.login(self.clv.usr, self.clv.pwd)
 		self.ftp.cwd('{}'.format(fecha))
+		for i in range(1, 6):
+			print("Descargando el Archivo d{}.txt".format(i))
+			self.ftp.retrbinary('RETR d{}.txt'.format(i),open('d{}.txt'.format(i),'wb').write)
+		self.ftp.quit()
+
+	def descDocs(self, fecha):
+		'''Descargar los 5 archivos respectivo a la fecha
+		ingresada por parametro'''
 		if os.path.exists('data'):
 			os.chdir('data')
 		else:
@@ -75,16 +90,11 @@ class DescargarArchivos(Login):
 			os.chdir('data')
 		if os.path.exists('{}'.format(fecha)):
 			os.chdir('{}'.format(fecha))
-			for i in range(1, 6):
-				print("Descargando el Archivo d{}.txt".format(i))
-				self.ftp.retrbinary('RETR d{}.txt'.format(i),open('d{}.txt'.format(i),'wb').write)
+			DescargarArchivos().descarga()
 		else:
 			os.mkdir('{}'.format(fecha))
 			os.chdir('{}'.format(fecha))
-			for i in range(1, 6):
-				print("Descargando el Archivo d{}.txt".format(i))
-				self.ftp.retrbinary('RETR d{}.txt'.format(i),open('d{}.txt'.format(i), 'wb').write)
-		self.ftp.quit()
+			DescargarArchivos().descarga()
 		os.chdir('../..')
 
 class Menu:
@@ -106,20 +116,38 @@ class Menu:
 	def run(self):
 		'''Muestra el menu y responde a la eleccion.'''
 		self.MostrarMenu()
-		eleccion = input("Selecciona la opcion: ")
-		accion = self.elecciones.get(eleccion)
-		if(accion):
-			accion()
-		else:
-			print("{} no es una eleccion valida".format(eleccion))
+		op = 1
+		while(op != 2):
+			eleccion = input("Selecciona la opcion: ")
+			accion = self.elecciones.get(eleccion)
+			if(accion):
+				accion()
+				op += 1
+			else:
+				print("{} no es una eleccion valida".format(eleccion))
 
 	def IngresarPara(self):
-		TproMin = int(input("Temperatura promedio Minima: "))
-		TproMax = int(input("Temperatura promedio Maxima: "))
-		Dpoint = int(input("Dpoint: "))
-		listaParametros[0] = TproMin
-		listaParametros[1] = TproMax
-		listaParametros[2] = Dpoint
+		validar = 0
+		while (validar != 1):
+			try:
+				lista.append(int(input("Temperatura promedio Minima: ")))
+				validar += 1
+			except ValueError:
+				print("Ingresa solo numeros")
+		while (validar != 2):
+			try:
+				lista.append(int(input("Temperatura promedio Maxima: ")))
+				validar += 1
+			except ValueError:
+				print("Ingresa solo numeros")
+		while (validar != 3):
+			try:
+				lista.append(int(input("Dpoint: ")))
+				validar += 1
+			except ValueError:
+				print("Ingresa soo numeros")
+		for i in range(0, 3):
+			Rango[i] = lista[i]
 
 	def Default(self):
 		pass
@@ -129,30 +157,83 @@ class Menu:
 		sys.exit(0)
 
 class DataFrame:
+	def roya(self, Tpro, Dpoint, NocFres):
+		'''Metodo para la generacion de valo 1 o 0 de cada fila del DataFrame,
+		Y poder pronosticar si la enfermedad roya se presenta en el dia'''
+		if (Tpro >= Rango[0] and Tpro <= Rango[1]  and Dpoint > Rango[2] and NocFres >= Rango[3] and NocFres <= Rango[4]):
+			return '1'
+		else:
+			return '0'
+
+	def indice(self, d1, d2, d3, d4, d5):
+		'''Metodo que evalua el grado de'''
+		rango = '{}{}{}{}{}'.format(d1,d2,d3,d4,d5)
+		if rango in rangos:
+			for i in range(0, len(rangos)):
+				if rangos[i] == rango:
+					indice = rangos.index(rangos[i])
+					return grado[indice]
+		else:
+			return 0
+
 	def BaseDataFrame(self, fecha):
 		'''Genera un DataFrame uniendo las variables (Tmax, Tmin, Tpro, Dpoint)
 		de los 5 archivos descargados desde el ftp'''
 		os.chdir('data/{}'.format(fecha))
+		print('El proceso de creacion de DataFrame a comensado espero unos segundos...')
+		df = pd.DataFrame()
 		for i in range(1, 6):
 			data = pd.read_csv("d{}.txt".format(i))
 			if i == 1:
-				df = data[['Long','Lat','WprSoil10_40']]
-			if i > 0:
-				df['Tpro{}'.format(i)] = data['Tpro']
-				df['NocFres{}'.format(i)] = (data['Tmax']-data['Tmin'])
-				df['Dpoint{}'.format(i)] = data['Dpoint']				
-		return df 
+				df['Lat'] = data['Lat']
+				df['Long'] = data['Long']
+				df['WprSoil10_40'] = data['WprSoil10_40']
+			df['Tpro{}'.format(i)] = data['Tpro']
+			df['NocFres{}'.format(i)] = (data['Tmax']-data['Tmin'])
+			df['Dpoint{}'.format(i)] = data['Dpoint']
+		df = df.loc[df['WprSoil10_40'] <= 99]
+		for j in range(1,6):
+			df['d{}'.format(j)] = df.apply(lambda x:DataFrame().roya(x['{}{}'.format(var[0],j)],
+				x['{}{}'.format(var[1],j)],x['{}{}'.format(var[2],j)]),axis=1)
+		df['Indice'] = df.apply(lambda x: DataFrame().indice(x['d1'], x['d2'], x['d3'], x['d4'], x['d5']), axis=1)
+		os.chdir('../..')
+		return df
 
-	def FiltarInformacion(self, dataFrame):
-		for i in range(1,6):
-			x, y = 'Long', 'Lat'
-			Long = np.array(dataFrame['{}'.format(x)])
-			Lat = np.array(dataFrame['{}'.format(y)])
-			Mexico = dataFrame.loc[dataFrame['WprSoil10_40'] <= 99]
-			Mexico = Mexico.loc[Mexico['Tpro{}'.format(i)] >= listaParametros[0]]
-			Mexico = Mexico.loc[Mexico['Tpro{}'.format(i)] <= listaParametros[1]]
-			Mexico = Mexico.loc[Mexico['Dpoint{}'.format(i)] > listaParametros[2]]
-			Mexico = Mexico.loc[(Mexico['Tmax{}'.format(i)] - Mexico['Tmin{}'.format(i)])]
+class Mapas:
+	def GenerarMapas(self, df, fecha, FehasArreglo):
+		if os.path.exists('mapas'):
+			os.chdir('mapas')
+		else:
+			os.mkdir('mapas')
+			os.chdir('mapas')
+		if os.path.exists('{}'.format(fecha)):
+			os.chdir('{}'.format(fecha))
+		else:
+			os.mkdir('{}'.format(fecha))
+			os.chdir('{}'.format(fecha))
+		Long = np.array(df['Long'])
+		Lat = np.array(df['Lat'])
+		for i in range (1, 7):
+			map = Basemap(projection='mill', resolution='c', llcrnrlon=Long.min(), llcrnrlat=Lat.min(), 
+				urcrnrlon=Long.max(), urcrnrlat=Lat.max())
+			if (i > 0 and i < 6):
+				roya = df.loc[df['d{}'.format(i)]=='1']
+				x, y = map(np.array(roya['Long']), np.array(roya['Lat']))
+				map.scatter(x, y, marker='.', color='green', s=1)
+				map.readshapefile('../../shapes/Estados', 'Mill')
+				print('Generando mapa del dia {}'.format(FehasArreglo[i-1]))
+				plt.title('Pronostico de ROYA \n del {}'.format(FehasArreglo[i-1]))
+				plt.savefig('Pronostico de ROYA del {}.png'.format(FehasArreglo[i-1]), dpi=300)
+				plt.clf()
+			if i == 6:
+				for j in range(1, 11):
+					roya = df.loc[df['Indice'] == j]
+					x, y = map(np.array(roya['Long']), np.array(roya['Lat']))
+					map.scatter(x, y, marker='.', color='{}'.format(colores[j-1]), s=1)
+				map.readshapefile('../../shapes/Estados', 'Mill')
+				print('Generando Mapa del pronostico de los 5 Dias')
+				plt.title('Pronostico de ROYA de los 5 Dias')
+				plt.savefig('Pronostico de ROYA de los 5 Dias', dpi=300)
 
 if __name__ == "__main__":
 	fecha = Fecha().obtencionFecha()
@@ -162,5 +243,5 @@ if __name__ == "__main__":
 	DescargarArchivos().descDocs(fecha)
 	print("*"*40)
 	Menu().run()
-	dataFrame = DataFrame().BaseDataFrame(fecha)
-	print(dataFrame.head(2))
+	df = DataFrame().BaseDataFrame(fecha)
+	Mapas().GenerarMapas(df,fecha,FehasArreglo)
