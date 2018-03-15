163de3f6505cbba608dbf048eb18e31b89617deb
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-03-15 10:32:51 -0600

    Estructura del proyecto

--- roya.py
+++ roya.py
@@ -16,7 +16,9 @@ def main():
     #fecha = obt_fecha(cve)
     cincodias = cinco_dias(fecha)
     #desc_docs(fecha,cve)
-    mapa_tot(fecha,cincodias)
+    roya = data_frame(fecha)
+    gen_mapas(roya, fecha, cinco_dias)
+
 def obt_fecha(cve): #Obtener la fecha actual
     fecha = []
     ftp = FTP(cve.ip) #Nombre del servidor
@@ -47,6 +49,7 @@ def cinco_dias(fecha): #Obtener cuatro dias posteriores a la fecha obtenida
                 dias.append('{:04d}-01-{:02d}'.format(ano + 1, n - (dias_mes - dia)))
     print('Lista de 5 dias generada: {}'.format(dias))
     return dias #Develve la lista de los 5 dias
+
 def desc_docs(fecha, cve): #Descargar los documentos de la carpeta con el nombre de la fecha actual
     ftp = FTP(cve.ip); #Nombre del servidor
     ftp.login(cve.usr, cve.pwd) #Usuario y contrasena del servidor
@@ -62,12 +65,14 @@ def desc_docs(fecha, cve): #Descargar los documentos de la carpeta con el nombre
         ftp.retrbinary('RETR d{}.txt'.format(i),open('d{}.txt'.format(i),'wb').write) #Descarga los documentos
     ftp.quit()
     os.chdir('../..') #Sale de la carpeta con la fecha/datos al directorio raiz
-def roya(Tpro,Dpoint,Noch_fres): #Funcion utilizada para devolver valores de 1/0 el lugar de Verdadero/Falso, en una declaracion posterior
+
+def modelo(Tpro,Dpoint,Noch_fres): #Funcion utilizada para devolver valores de 1/0 el lugar de Verdadero/Falso, en una declaracion posterior
     if Tpro >= 25 and Tpro <=30 and Dpoint > 5 and Noch_fres >= 15 and Noch_fres <=20: #Filtrado de datos donde se presenta la ROYA
         return 1
     else:
         return 0
-def indice(d1,d2,d3,d4,d5):
+
+def indice(d1,d2,d3,d4,d5): #Funcion utilizada para generar un indice que determinara el grado de impacto de la ROYA en base al modelo
     if d1==1 and d2==1 and d3==1 and d4==1 and d5==1:
         return 10 
     elif d1==0 and d2==1 and d3==1 and d4==1 and d5==1:
@@ -90,7 +95,8 @@ def indice(d1,d2,d3,d4,d5):
         return 1 
     else:
         return 0
-def mapa_tot(fecha, cincodias): #Generacion del Pronostico de ROYA en los 5 fias
+
+def data_frame(fecha): #Generacion del Pronostico de ROYA en los 5 fias
     df = pd.DataFrame() #Declaracion del DataFrame
     for i in range (1, 6): #Ciclo donde se prosesan los 5 documentos
         datos = pd.read_csv('datos/{}/d{}.txt'.format(fecha, i)) #Almacenar dato correspondiente en datos
@@ -105,21 +111,23 @@ def mapa_tot(fecha, cincodias): #Generacion del Pronostico de ROYA en los 5 fias
     Long, Lat = np.array(df['Long']), np.array(df['Lat'])
     Long_min, Long_max, Lat_min, Lat_max = Long.min(), Long.max(), Lat.min(), Lat.max() 
     df = df.loc[df['WprSoil10_40'] <= 99]
+    variables = ['Tpro','Dpoint','Noch_fres'] #Lista con las variables a utilizar
+    for i in range (1,6): #Cliclo utilizado para crear 5 columnas (1 por cada filtro de variables), utilizando la funcion "modelo" para determinar que filas cumplen las condiciones
+        df['d{}'.format(i)] = df.apply(lambda x:modelo(x['{}{}'.format(variables[0],i)],x['{}{}'.format(variables[1],i)],x['{}{}'.format(variables[2],i)]),axis=1)
+    df['indice'] = df.apply(lambda x:indice(x['d1'],x['d2'],x['d3'],x['d4'],x['d5']),axis=1)
+    return df
+
+def gen_mapas(roya, fecha, cincodias)
     if not os.path.exists('mapas'): #Verifica si la carpeta mapas existe (donde se almacenaran los documentos a descargar)
         os.mkdir('mapas') #Crea la carpeta mapas
     os.chdir('mapas') #Accede a la carpeta mapas
     if not os.path.exists('{}'.format(fecha)): #Verificar si la carpeta fecha existe que es donde se almacenaran los mapas
         os.mkdir('{}'.format(fecha)) #Crea la carpeta fecha donde se almacenaran los documentos
     os.chdir('{}'.format(fecha)) #Ingresar a la carpeta fecha
-    variables = ['Tpro','Dpoint','Noch_fres'] #Lista con las variables a utilizar
-    
-    for i in range (1,6): #Cliclo utilizado para crear 5 columnas (1 por cada filtro de variables), utilizando la funcion "roya" para determinar que filas cumplen las condiciones
-        df['d{}'.format(i)] = df.apply(lambda x:roya(x['{}{}'.format(variables[0],i)],x['{}{}'.format(variables[1],i)],x['{}{}'.format(variables[2],i)]),axis=1)
-    df['indice'] = df.apply(lambda x:indice(x['d1'],x['d2'],x['d3'],x['d4'],x['d5']),axis=1)
-    
+       
     '''for i in range (1, 6):
         map = Basemap(projection='mill', resolution='c', llcrnrlon=Long.min(), llcrnrlat=Lat.min(), urcrnrlon=Long.max(), urcrnrlat=Lat.max())
-        var = df.loc[df['d{}'.format(i)]==1]
+        var = roya.loc[roya['d{}'.format(i)]==1]
         Eje_x, Eje_y = np.array(var['Long']), np.array(var['Lat'])
         x, y = map(Eje_x, Eje_y)
         map.scatter(x, y, marker='.',color='#00FF00',s=1)
@@ -132,7 +140,7 @@ def mapa_tot(fecha, cincodias): #Generacion del Pronostico de ROYA en los 5 fias
     colores = ['#00FF00','#00FF00','#00FF00','#00FF00','#FFFF00','#FFFF00','#FFFF00','#FF8000','#FF8000','#FF0000']
     map = Basemap(projection='mill', resolution='c', llcrnrlon=Long.min(), llcrnrlat=Lat.min(), urcrnrlon=Long.max(), urcrnrlat=Lat.max())
     for i in range (1, 11):
-        var = df.loc[df['indice']==i]  
+        var = roya.loc[roya['indice']==i]  
         Eje_x, Eje_y = np.array(var['Long']), np.array(var['Lat'])
         x, y = map(Eje_x, Eje_y)
         map.scatter(x, y, marker='.',color='{}'.format(colores[i-1]),s=1)
@@ -142,17 +150,7 @@ def mapa_tot(fecha, cincodias): #Generacion del Pronostico de ROYA en los 5 fias
     plt.title('Pronostico de ROYA \n De {} a {}'.format(cincodias[0], cincodias[4]))
     plt.savefig(fname="Pronostico_ROYA_{}_a_{}.png".format(cincodias[0], cincodias[4]), dpi=300)
     os.chdir('../..') #Sale de la carpeta con la fecha/datos al directorio raiz
-    '''
-    map = Basemap(projection='mill', resolution='c', llcrnrlon=Long.min(), llcrnrlat=Lat.min(), urcrnrlon=Long.max(), urcrnrlat=Lat.max())
-    x, y = map(np.array(df['Long']), np.array(df['Lat']))
-    numCols=len(x)
-    numRows=len(y)
-    xi = np.linspace(x.min(), x.max(), numCols)
-    yi = np.linspace(y.min(), y.max(), numRows)
-    xi, yi = np.meshgrid(xi, yi)
-    z = np.array(df['indice'])
-    zi = gd((x,y), z, (xi,yi), method='cubic')
-    mapa = map.contourf(xi, yi, zi, cmap='RdYlGn')
-    cbar = map.colorbar(cs, location='rigth', pad='5%')
+    '''    
+    
 if __name__=="__main__":
     main()
